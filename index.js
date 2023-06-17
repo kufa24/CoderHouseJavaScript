fdb831fa96ffef1aa3e343599b7d3de508e41e51
kufa24 <fspenasoft@gmail.com>
2023-06-17 16:02:09 -0300

    Pre Entrega 3

--- index.js
+++ index.js
@@ -1,157 +1,124 @@
-/* 
+// Funciones para realizar calculos
 
-Apenas se abre el primer propmt ingresan Salir, luego F5 y a partir de ahi funciona como deberia. Hasta que no ingresen Salir y despues F5 no va a mostrar nada en consola.
 
-Saludos.-
-
-*/
-
-console.log("¡Bienvendio a Seguros Tusam!");
-console.log("");
-console.log("A continuacion, calcularemos el valor de tu seguro...");
-console.log("");
-
-// FUNCIONES
-
-function calcular_auto( monto ){
-    suma_asegurada = parseFloat(monto);
-    suma_asegurada = suma_asegurada - (suma_asegurada * 0.85);
-    return suma_asegurada
+function calcular_auto(monto) {
+    monto = monto - (monto * 0.85);
+    return monto;
 }
 
-function calcular_moto( monto ){
-    suma_asegurada = parseFloat(monto);
-    suma_asegurada = suma_asegurada - (suma_asegurada * 0.80);
-    return suma_asegurada
-}
-function calcular_bicicleta( monto ){
-    suma_asegurada = parseFloat(monto);
-    suma_asegurada = suma_asegurada - (suma_asegurada * 0.75);
-    return suma_asegurada
-}
-function calcular_lancha( monto ){
-    suma_asegurada = parseFloat(monto);
-    suma_asegurada = suma_asegurada - (suma_asegurada * 0.70);
-    return suma_asegurada
+function calcular_moto(monto) {
+    monto = monto - (monto * 0.80);
+    return monto;
 }
 
-function valor_descuento (valor, descuento){
+function calcular_bicicleta(monto) {
+    monto = monto - (monto * 0.75);
+    return monto;
+}
+
+function calcular_lancha(monto) {
+    monto = monto - (monto * 0.70);
+    return monto;
+}
+
+function valor_descuento(valor, descuento) {
     let valor_final;
-    if(descuento === "Si"){
+    if (descuento === "Si") {
         valor_final = valor - (valor * 0.10);
-    } else{
+    } else {
         valor_final = valor;
     }
     return valor_final;
 }
 
-//OBJETOS
+// Objeto para enviar a la lista
 
 class ElementosAsegurados {
 
-    constructor(objeto_asegurado, monto, total){
-        this.objeto_asegurado = objeto_asegurado;
+    constructor(elementoAsegurado, monto, total){
+        this.elementoAsegurado = elementoAsegurado;
         this.monto = monto;
         this.total = total;
     }
-    datosSeguros(){
-        console.log("----> DATOS DE SEGURO SOLICITADO <----")
-        console.log("Objeto a Asegurar: ", this.objeto_asegurado);
-        console.log("Valor ingresado: $", this.monto);
-        console.log("Monto a Pagar: $", this.total);
-    }
 }
 
-// LISTAS
+// Lista
 
 let lista_seguros = [];
 
-// BUCLE PROMPT --> SOLICITUD DE DATOS PARA CALCULAR VALOR DEL SEGURO
+// Funcion boton 'REALIZAR CALCULO'
 
-let ingreso = "";
+function realizarCalculo() {
 
-while( ingreso != "Salir"){
-    
-    ingreso = prompt("Escriba una de las siguientes opciones: Seguros / Salir");
-    
-    if(ingreso === "Seguros"){
-        
-        let objeto_asegurado = prompt("Escriba que desea asegurar: Auto / Moto / Bicicleta / Lancha");
-        
-        let monto = parseFloat(prompt("Ingrese el monto a asegurar(solo numeros): "));
-        
-        if(objeto_asegurado === "Auto"){
-            
-            let valor_cuota_auto = calcular_auto( monto );
-            
-            let descuento = prompt("¿Usted cuenta con una flota de seguros? Ingrese Si o No");
-            
-            let total = valor_descuento(valor_cuota_auto, descuento);
+    // Captura de datos ingresados por el usuario
+    let elementoNombre = document.getElementById("nombre");
+    let elementoApellido = document.getElementById("apellido");
+    let elementoElementoAsegurado = document.getElementById("elementoAsegurado");
+    let elementoMonto = document.getElementById("monto");
+    let elementoDescuento = document.getElementById("descuento");
 
-            let ElementosAsegurados1 = new ElementosAsegurados(objeto_asegurado, monto, total);
-            
-            lista_seguros.push(ElementosAsegurados1);
+    let nombre = elementoNombre.value;
+    let apellido = elementoApellido.value;
+    let elementoAsegurado = elementoElementoAsegurado.value;
+    let monto = elementoMonto.value;
+    let descuento = elementoDescuento.value;
 
-            ElementosAsegurados1.datosSeguros();
-            
-        } else if(objeto_asegurado === "Moto"){
-            
-            let valor_cuota_moto = calcular_moto( monto );
-            
-            let descuento = prompt("¿Usted cuenta con una flota de seguros? Ingrese Si o No");
-            
-            let total = valor_descuento(valor_cuota_moto, descuento);
-            
-            let ElementosAsegurados1 = new ElementosAsegurados(objeto_asegurado, monto, total);
-            
-            lista_seguros.push(ElementosAsegurados1);
+    // Local Storage
 
-            ElementosAsegurados1.datosSeguros()
-            
-        } else if(objeto_asegurado === "Bicicleta"){
-            
-            let valor_cuota_bicicleta = calcular_bicicleta( monto );
-            
-            let descuento = prompt("¿Usted cuenta con una flota de seguros? Ingrese Si o No");
-            
-            let total = valor_descuento(valor_cuota_bicicleta, descuento);
-            
-            let ElementosAsegurados1 = new ElementosAsegurados(objeto_asegurado, monto, total);
-            
-            lista_seguros.push(ElementosAsegurados1);
+    localStorage.setItem("nombreGuardado", nombre);
+    localStorage.setItem("apellidoGuardado", apellido);
+    let nombreRecuperado = localStorage.getItem("nombreGuardado");
+    let apellidoRecuperado = localStorage.getItem("apellidoGuardado");
 
-            ElementosAsegurados1.datosSeguros()
-            
-        } else if(objeto_asegurado === "Lancha"){
-            
-            let valor_cuota_lancha = calcular_lancha( monto );
-            
-            let descuento = prompt("¿Usted cuenta con una flota de seguros? Ingrese Si o No");
-            
-            let total = valor_descuento(valor_cuota_lancha, descuento);
-            
-            let ElementosAsegurados1 = new ElementosAsegurados(objeto_asegurado, monto, total);
-            
-            lista_seguros.push(ElementosAsegurados1);
+    console.log(nombreRecuperado);
+    console.log(apellidoRecuperado);
 
-            ElementosAsegurados1.datosSeguros()
-            
-        } else{
-            console.log("Dato Incorrecto");
-        }
-    }else if(ingreso === "Salir"){
-        console.log("¡HASTA LA PROXIMA!");
+
+    // If par los distintos tipos de Seguros donde aplican las funciones
+
+    if(elementoAsegurado === "Auto"){
+        let valor_cuota_auto = calcular_auto( monto );
+        let total = valor_descuento(valor_cuota_auto, descuento);
+        let ElementosAsegurados1 = new ElementosAsegurados(elementoAsegurado, monto, total);
+        lista_seguros.push(ElementosAsegurados1);
+    } else if(elementoAsegurado === "Moto"){
+        let valor_cuota_moto = calcular_moto( monto );
+        let total = valor_descuento(valor_cuota_moto, descuento);
+        let ElementosAsegurados1 = new ElementosAsegurados(elementoAsegurado, monto, total);
+        lista_seguros.push(ElementosAsegurados1);
+    } else if(elementoAsegurado === "Bicicleta"){
+        let valor_cuota_bicicleta = calcular_bicicleta( monto );
+        let total = valor_descuento(valor_cuota_bicicleta, descuento);
+        let ElementosAsegurados1 = new ElementosAsegurados(elementoAsegurado, monto, total);
+        lista_seguros.push(ElementosAsegurados1);
     } else{
-        console.log("Dato Incorrecto");
+        let valor_cuota_lancha = calcular_lancha( monto );
+        let total = valor_descuento(valor_cuota_lancha, descuento);
+        let ElementosAsegurados1 = new ElementosAsegurados(elementoAsegurado, monto, total);
+        lista_seguros.push(ElementosAsegurados1);
     }
-    console.log("¡FINALIZADO!");
-    console.log("");
+
+     // Ultimo dato cargado de la Lista
+    let datoLista = lista_seguros[lista_seguros.length - 1];
+
+    let tabla = document.getElementById("tabla");
+
+    // Insertar fila dentro de la tabla
+    let fila = tabla.insertRow();
+
+    // Insertar celdas en la fila
+    let celdaElemento = fila.insertCell();
+    let celdaMonto = fila.insertCell();
+    let celdaTotal = fila.insertCell();
+
+    // Insertar los valores dentro de las celdas de la tabla
+    celdaElemento.innerHTML = datoLista.elementoAsegurado;
+    celdaMonto.innerHTML = "$" + datoLista.monto;
+    celdaTotal.innerHTML = "$" + datoLista.total;
+
+    // Ir hasta el final del html
+
+    let footer = document.getElementById("footer");
+    footer.scrollIntoView({ behavior: "smooth", block: "end" });
 }
 
-// MUESTRA TOTAL FINAL
-
-console.log("A continuacion le mostraremos la suma total a abonar por mes de sus seguros:")
-
-let sumatoriaTotal = lista_seguros.reduce((acumulador, elemento) => acumulador  + elemento.total, 0);
-
-console.log("Suma total a Abonar: $ ", sumatoriaTotal);
